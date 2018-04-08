c32708c032e29aded249828c261b0deb889932d5
mfouda214 <m.fouda@robone.net>
2018-04-08 14:55:00 +0100

    Add showDetails actions

--- UserDefaultsDemo/ViewController.swift
+++ UserDefaultsDemo/ViewController.swift
@@ -9,7 +9,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UIPopoverPresentationControllerDelegate  {
 
     @IBOutlet weak var firstNameTextField: UITextField!
     @IBOutlet weak var lastNameTextField: UITextField!
@@ -19,11 +19,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let userDefaults = UserDefaults.standard
-        firstNameTextField.text = userDefaults.object(forKey: "FirstName") as? String
-        lastNameTextField.text = userDefaults.object(forKey: "LastName") as? String
-        ageTextField.text = userDefaults.object(forKey: "Age") as? String
-        
     }
     
     @IBAction func saveButtonWasPressed(_ sender: UIButton) {
@@ -31,6 +26,12 @@ class ViewController: UIViewController {
         userDefaults.set(firstNameTextField.text, forKey: "FirstName")
         userDefaults.set(lastNameTextField.text, forKey: "LastName")
         userDefaults.set(ageTextField.text, forKey: "Age")
+        
+        // MARK - reset textFields
+        
+        firstNameTextField.text = ""
+        lastNameTextField.text = ""
+        ageTextField.text = ""
         
 //        userDefaults.synchronize()
     }
@@ -40,5 +41,69 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func ShowUserDetails(_ sender: Any) {
+        
+        let storyboard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "InformationViewController")
+        
+        viewController.modalPresentationStyle = .popover
+        let popover: UIPopoverPresentationController = viewController.popoverPresentationController!
+        popover.barButtonItem = sender as? UIBarButtonItem
+        popover.delegate = self
+        present(viewController, animated: true, completion:nil)
+        
+    }
+    
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        return .fullScreen
+    }
+    
+    func presentationController(_ controller: UIPresentationController, viewControllerForAdaptivePresentationStyle style: UIModalPresentationStyle) -> UIViewController? {
+        let navigationController = UINavigationController(rootViewController: controller.presentedViewController)
+        let doneButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(ViewController.dismissViewController))
+        navigationController.topViewController?.navigationItem.rightBarButtonItem = doneButton
+        return navigationController
+        
+    }
+    
+    @objc func dismissViewController() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func ResetUserDetails(_ sender: Any) {
+        
+        let alertController = UIAlertController(title: "Confirm?", message: "Confirm Reset action, please!", preferredStyle: .actionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (alert) in
+            print("User tapped Cancel")
+        }
+        
+        let yesAction = UIAlertAction(title: "Yes", style: .default, handler: { (alert) in
+            print("User tapped Yes")
+            
+            // MARK - reset textFields
+            
+            self.firstNameTextField.text = ""
+            self.lastNameTextField.text = ""
+            self.ageTextField.text = ""
+            
+            let userDefaults = UserDefaults.standard
+            userDefaults.set(self.firstNameTextField.text, forKey: "FirstName")
+            userDefaults.set(self.lastNameTextField.text, forKey: "LastName")
+            userDefaults.set(self.ageTextField.text, forKey: "Age")
+        
+        })
+        
+        alertController.addAction(cancelAction)
+        alertController.addAction(yesAction)
+        
+        // solve iPad Alerts view
+        alertController.popoverPresentationController?.sourceRect = (sender as AnyObject).frame
+        alertController.popoverPresentationController?.sourceView = view
+        
+        present(alertController, animated: true, completion: nil)
+        
+    }
+    
 }
 
